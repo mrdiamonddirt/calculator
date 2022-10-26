061cf42e2da556e5cede72cbbc760c9b9acb7ee6
RowDog <mrdiamonddirt@gmail.com>
2022-10-26 11:26:33 +0100

    cancel, evaluate, and updatecount functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 // load math.js (using node.js)
-import {useState} from 'react'
+import {evaluate} from 'mathjs'
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
-  const buttonnums  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+  const buttonnums  = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-','*', '/', '.', '=', 'c']
+
+  //create handler to handle adding characters to the display array
+  function addnumhander(button) {
+    setCount(count + button)
+  //use state trigger on first interaction slice 0 from array
+    if (button === 'c') {
+      setCount(0)
+    } else if ( button === '=' ) {
+      setCount(evaluate(count)) 
+    }
+  }
   return (
     <>
     <Calculator display={count} addnum={setCount} />
-    <Buttons array={buttonnums} btntotal={count} btncnt={setCount}/>
+    <Buttons array={buttonnums} btntotal={count} btncnt={addnumhander}/>
     </>
   );
 }
-
+//
 function Calculator(props) {
   return(
     <h1>{props.display}</h1>
   )
 }
+
+//
 function Buttons(props) {  
   return (
   <>
